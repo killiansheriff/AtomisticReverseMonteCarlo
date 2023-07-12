bda2b3ed8291f51c0a1b0b0e0b7d2e81f1046afa
Killian Sheriff <ksheriff@mit.edu>
2023-07-12 12:20:43 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="rmc",
+    version="0.0.1",
+    author="Killian Sheriff",
+    author_email="ksheriff@mit.edu",
+    description="Reverse Monte Carlo",
+    url="https://github.com/killiansheriff/rmc",
+    packages=["rmc"],
+    install_requires=[],
+    classifiers=[],
+)
